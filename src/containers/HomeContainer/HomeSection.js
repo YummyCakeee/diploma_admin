2b5920064727c6ea3a8d02ab84a15c38b84046c4
Nikita <nikon15151@yandex.ru>
2022-04-30 16:24:33 +0300

    upd: separator as external component

--- src/containers/HomeContainer/HomeSection.js
+++ src/containers/HomeContainer/HomeSection.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+import SectionSeparator from "components/Elements/SectionSeparator/SectionSeparator";
 
 const HomeSection = ({ children }) => {
     return (
         <View style={styles.container}>
             {children}
-            <LinearGradient
-                colors={[
-                    'rgba(255, 255, 255, 0)',
-                    'rgba(255, 255, 255, 1)',
-                    'rgba(255, 255, 255, 0)',
-                ]}
-                start={{x: 0.1, y: 0.5}}
-                end={{x: 0.9, y: 0.5}}
-                style={styles.border}
-            />
+            <SectionSeparator/>
         </View>
     )
 }
@@ -24,11 +15,6 @@ const styles = StyleSheet.create({
     container: {
         marginVertical: 5,
     },
-    border: {
-        width: '100%',
-        height: 2,
-        opacity: 0.5,
-    }
 })
 
 export default HomeSection

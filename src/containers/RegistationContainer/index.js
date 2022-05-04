48afb9c1bb3e9c38d71ba78f2624eaf61d27b26e
Nikita <nikon15151@yandex.ru>
2022-05-04 17:54:49 +0300

    upd: tokens check on init

--- src/containers/RegistationContainer/index.js
+++ src/containers/RegistationContainer/index.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import PageTemplate from "components/PageTemplate/Pagetemplate"
 import globalStyles from "global/styles/styles"
 import { StyleSheet, Text, View } from "react-native"
 import { useNavigation } from "@react-navigation/core"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import Authorization from "containers/Forms/Authorization"
 import Registration from "containers/Forms/Registration"
+import { updateUser } from "store/actions/userSlice"
+import { store } from "store"
 export const AUTH_TYPE = 'auth_type'
 export const REG_TYPE = 'reg_type'
 
 const RegistationContainer = () => {
+    useEffect(()=> {
+        const getTokens = async () => {
+
+            const authToken = await AsyncStorage.getItem('authToken')
+            const refreshToken = await AsyncStorage.getItem('refreshToken')
+            if (authToken && refreshToken) {
+                const userData = {
+                    authToken,
+                    refreshToken
+                }
+                store.dispatch(updateUser(userData))
+                onAuthSuccess()
+            }
+        }
+        getTokens()
+    }, [])
+
     const [signType, setSignType] = useState(AUTH_TYPE)
     const navigation = useNavigation()
 
@@ -23,14 +43,12 @@ const RegistationContainer = () => {
             index: 0, 
             routes: [{name: 'Feed'}]
         })
-        //navigation.navigate('Feed')
     }
     const onRegSuccess = () => {
         navigation.reset({
             index: 0, 
             routes: [{name: 'Feed'}]
         })
-        navigation.navigate('Feed')
     }
 
     return (

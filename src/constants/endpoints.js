4f2a79ad183e58d3e72d22521236774ca230f25e
Nikita <nikon15151@yandex.ru>
2022-05-04 16:01:41 +0300

    upd: endpoints routes

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,3 @@
-export const ENDPOINT_MAIN_REG = '/users/signup'
-export const ENDPOINT_MAIN_AUTH = '/users/auth/signin'
+export const ENDPOINT_MAIN_REG = '/api/users/auth/signup'
+export const ENDPOINT_MAIN_AUTH = '/api/users/auth/signin'
 export const ENDPOINT_USER_UPDATE = ''

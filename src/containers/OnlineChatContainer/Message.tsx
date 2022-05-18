7e8d1d42713fa68e9ad7bb875d333b3622948184
Nikita <nikon15151@yandex.ru>
2022-05-18 22:55:24 +0300

    upd: on press event, dates splitter

--- src/containers/OnlineChatContainer/Message.tsx
+++ src/containers/OnlineChatContainer/Message.tsx
@@ -1,46 +1,63 @@
-import React, { useState, useEffect } from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { Color } from "global/styles/constants"
+import globalStyles from "global/styles/styles"
+import React, { useState, useRef, useEffect } from "react"
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity, StyleProp } from "react-native"
+import LinearGradient from "react-native-linear-gradient"
+import ContextMenu from "components/Elements/ContextMenu/ContextMenu"
 
 type messageProps = {
     text: string,
     date: Date,
+    read: boolean,
     fromThisUser: boolean,
     thisUserOnLeft: boolean,
     thisUserMessageStyle: {},
     otherUserMessageStyle: {},
-    style?: {}
+    onPress: (measure: {}) => void
+    style?: StyleProp<View>
 }
 
 const Message: React.FC<messageProps> = ({
     text = '',
     date = new Date(),
+    read = false,
     fromThisUser = false,
     thisUserOnLeft = false,
     thisUserMessageStyle,
     otherUserMessageStyle,
+    onPress = () => { },
     style
 }) => {
-
     const [messageTime, setMessageTime] = useState('')
-
+    const ref = useRef<TouchableOpacity>(null)
     useEffect(() => {
-        const time = date?.toLocaleTimeString('ru-RU').replace(/:\d+/, '')
+        const time = date?.toLocaleTimeString('ru-RU').replace(/:\d+$/, '')
         setMessageTime(time)
     }, [date])
 
+    const onMessagePress = () => {
+        ref.current?.measure((x, y, w, h, pX, pY) => onPress({x, y, w, h, pX, pY}))
+    }
+
     return (
-        <View
+        <TouchableOpacity
+            ref={ref}
+            activeOpacity={1}
+            onPress={onMessagePress}
             style={[
                 styles.container,
                 {
                     alignSelf: fromThisUser ? !thisUserOnLeft ?
-                    'flex-end': 'flex-start' : !thisUserOnLeft ?
-                    'flex-start' : 'flex-end',
+                        'flex-end' : 'flex-start' : !thisUserOnLeft ?
+                        'flex-start' : 'flex-end',
                 },
+                fromThisUser ? thisUserMessageStyle : otherUserMessageStyle,
                 style,
-                fromThisUser ? thisUserMessageStyle : otherUserMessageStyle
             ]}
         >
+            {fromThisUser && !read && (
+                <View style={styles.messageUnreadStatus}></View>
+            )}
             <View
                 style={styles.messageTextContainer}
             >
@@ -59,7 +76,41 @@ const Message: React.FC<messageProps> = ({
                     {messageTime}
                 </Text>
             </View>
+        </TouchableOpacity>
+    )
+}
 
+export const MessagesDateSplitter: React.FC<{ date: Date }> = ({ date }) => {
+    return (
+        <View
+            style={styles.splitterContainer}
+        >
+            <LinearGradient
+                colors={[
+                    'rgba(100, 100, 100, 0)',
+                    'rgba(100, 100, 100, 1)'
+                ]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={styles.splitterGradient}
+            />
+            <Text
+                style={[
+                    globalStyles.text,
+                    styles.splitterText
+                ]}
+            >
+                {date}
+            </Text>
+            <LinearGradient
+                colors={[
+                    'rgba(100, 100, 100, 1)',
+                    'rgba(100, 100, 100, 0)'
+                ]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={styles.splitterGradient}
+            />
         </View>
     )
 }
@@ -75,7 +126,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        alignContent: 'flex-end'        
+        alignContent: 'flex-end',
     },
     messageTextContainer: {
         paddingHorizontal: 3,
@@ -94,7 +145,31 @@ const styles = StyleSheet.create({
     messageDate: {
         color: 'rgb(230, 230, 230)',
         fontSize: 12,
-    }
+    },
+    messageUnreadStatus: {
+        position: "absolute",
+        left: -20,
+        bottom: 15,
+        backgroundColor: Color.OceanBlue,
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+    },
+    splitterContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+    },
+    splitterText: {
+        fontSize: 12,
+        marginHorizontal: 5,
+    },
+    splitterGradient: {
+        width: 100,
+        height: 1
+    },
 })
 
 export default Message

410d426d1616017bd2c9c8a8386a81eb64d65d85
Nikita <nikon15151@yandex.ru>
2022-05-27 16:23:26 +0300

    add: online chat room list screen

---
+++ src/screens/onlineChatRoomListScreen/OnlineChatRoomList.js
@@ -0,0 +1,10 @@
+import React from "react"
+import OnlineChatRoomListContainer from "containers/OnlineChatRoomListContainer"
+
+const OnlineChatRoomList = () => {
+    return (
+        <OnlineChatRoomListContainer />
+    )
+}
+
+export default OnlineChatRoomList

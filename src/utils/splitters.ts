0bb116945b1dd20b04b272f6db870392a8abddf4
Nikita <nikon15151@yandex.ru>
2022-05-27 16:28:34 +0300

    add: date and time splitter

---
+++ src/utils/splitters.ts
@@ -0,0 +1,15 @@
+export const dateTimeSplitter = (value: Date): { date: string, time: string } => {
+    const match = value.toISOString()
+        .match(/([\w | \-]*)T([\w | \:]*):[\w | \.]*Z/)
+    if (match && match.length >= 3) {
+        const [date, time] = match?.slice(1)
+        return {
+            date,
+            time
+        }
+    }
+    return  {
+        date: 'ff',
+        time: 'ff'
+    }
+}

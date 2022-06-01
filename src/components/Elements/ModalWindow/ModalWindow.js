4b2dafb9dbb8bb11dded4a716e56f9380375f56f
Nikita <nikon15151@yandex.ru>
2022-06-01 23:59:11 +0300

    fix: addaptive size

--- src/components/Elements/ModalWindow/ModalWindow.js
+++ src/components/Elements/ModalWindow/ModalWindow.js
@@ -88,10 +88,11 @@ const styles = StyleSheet.create({
     container: {
         position: 'absolute',
         zIndex: 5000,
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
         backgroundColor: 'rgba(0, 0, 0, 0.4)',
         top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",

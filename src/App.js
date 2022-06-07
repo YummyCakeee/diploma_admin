29aafbc87e45019ae9260c10c5bc1d9c4e376888
Nikita <nikon15151@yandex.ru>
2022-06-07 21:17:23 +0300

    upd: global styles context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import AppNavigation from 'components/AppNavigation/AppNavigation';
 import StoreWrapper from 'store';
-import {LogBox } from 'react-native';
+import { LogBox } from 'react-native';
+import GlobalStylesWrapper from 'global/styles/GlobalStylesWrapper';
 LogBox.ignoreLogs(['Reanimated 2']);
 
 const App = () => {
   return (
     <StoreWrapper>
-      <AppNavigation />
+      <GlobalStylesWrapper>
+        <AppNavigation />
+      </GlobalStylesWrapper>
     </StoreWrapper>
   )
 }
